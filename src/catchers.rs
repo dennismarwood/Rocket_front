0669b5e7ebf8d3a60ea81e01f36fc2cc0c7b7c44
Dennis Marwood <dennismarwood@gmail.com>
2023-06-12 11:01:16 -0700

    Add general level 404s.

--- src/catchers.rs
+++ src/catchers.rs
@@ -2,22 +2,31 @@
 //use rocket::http::Status;
 //use rocket_dyn_templates::{Template, context};
 
-pub mod routes {
-    //use super::*;
 
-    // #[catch(default)]
-    // pub fn default_catcher(status: Status, request: &Request) -> Template { 
-    //     //println!("\n\n{:?}", status);
-    //     //println!("\n\n{:?}", request);
-    //     println!("\ndefault_catcher ran.");
-    //     Template::render("error/default", context! {})
-    // }
-    
-    // #[catch(500)]
-    // pub fn five_hundread(status: Status, request: &Request) -> Template {
-    //     //println!("\n\n{:?}", status);
-    //     //println!("\n\n{:?}", request);
-    //     println!("\nfive_hundread catcher ran.");
-    //     Template::render("error/default", context! {})
-    // }
+//use super::*;
+
+// #[catch(default)]
+// pub fn default_catcher(status: Status, request: &Request) -> Template { 
+//     //println!("\n\n{:?}", status);
+//     //println!("\n\n{:?}", request);
+//     println!("\ndefault_catcher ran.");
+//     Template::render("error/default", context! {})
+// }
+
+// #[catch(500)]
+// pub fn five_hundread(status: Status, request: &Request) -> Template {
+//     //println!("\n\n{:?}", status);
+//     //println!("\n\n{:?}", request);
+//     println!("\nfive_hundread catcher ran.");
+//     Template::render("error/default", context! {})
+// }
+
+#[catch(404)]
+pub fn not_found() -> &'static str {
+    "Gen 404"
+}
+
+#[catch(404)]
+pub fn post_not_found() -> &'static str {
+    "POST Gen 404"
 }
