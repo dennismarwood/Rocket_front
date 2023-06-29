b9aac72eac9ad20d52ec9146d6a20cfd2530d40f
Dennis Marwood <dennismarwood@gmail.com>
2023-06-29 16:22:01 -0700

    Add new() method for Response.

--- src/models.rs
+++ src/models.rs
@@ -1,6 +1,7 @@
 use serde::{Serialize, Deserialize};
 use serde::de::DeserializeOwned;
 use crate::errors::{ResponseError};
+use reqwest;
 
 #[derive(FromForm, Serialize, Deserialize, Debug)]
 pub struct User {
@@ -30,15 +31,24 @@ pub struct Response {
     pub data: Option<serde_json::Value>,
     pub code: Option<String>,
     pub errors: Option<serde_json::Value>,
+    pub status_code: Option<u16>,
 }
 
 impl Response {
+    pub async fn new(r: reqwest::Response) -> Result<Self, ResponseError> {
+        println!("{:?}", r.text().await);
+        todo!();
+        let status_code = r.status().as_u16();
+        let mut response = r.json::<Self>().await.map_err(|e| ResponseError::DeserializeError(e))?;
+        response.status_code = Some(status_code);
+        Ok(response)
+    }
+
     pub async fn get_data<T: DeserializeOwned>(r: reqwest::Response) -> Result<T, ResponseError> {
         //Expect a response from the back end to include Some(serde::Value) in the Response.data field.
         
         //Convert response from the back end into a Response struct.
-        let resp: Self = r.json::<Self>().await.map_err(|e| ResponseError::ReqwestError(e))?;
-
+        let resp: Self = Self::new(r).await?;
         //Extract serde Value from the Response.data field.
         let data = match resp.data {
             Some(data) => data,
@@ -51,7 +61,6 @@ impl Response {
         Ok(deserialized_data)
     }
 }
-
 
 
 #[derive(FromForm, Serialize, Deserialize)]
@@ -73,12 +82,32 @@ pub struct PasswordUpdate<'r> {
 
 #[derive(Serialize, Deserialize, Default, Clone, Debug)]
 pub struct Post {
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub id: Option<i32>,
+
+    pub title: Option<String>,
+    
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub author: Option<String>,
+    
+    #[serde(skip_serializing_if = "Option::is_none")] 
+    pub created: Option<chrono::NaiveDateTime>,
+   
+    //#[serde(skip_serializing_if = "Option::is_none")]
+    pub last_updated: Option<chrono::NaiveDate>,
+    
+    pub content: Option<String>,
+}
+
+#[derive(Serialize, Deserialize, Default, Clone, Debug, FromForm)]
+pub struct PostForm {
     pub id: Option<i32>,
     pub title: Option<String>,
     pub author: Option<String>,
-    pub created: Option<chrono::NaiveDateTime>,
-    pub last_updated: Option<chrono::NaiveDate>,
+    pub created: Option<String>,
+    pub last_updated: Option<String>,
     pub content: Option<String>,
+    pub tags: Option<Vec<String>>,
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
