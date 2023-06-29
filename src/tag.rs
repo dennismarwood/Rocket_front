24681870f22d18479342685aaad34b69871c9c73
Dennis Marwood <dennismarwood@gmail.com>
2023-06-29 16:22:52 -0700

    Add tag file placeholder.

---
+++ src/tag.rs
@@ -0,0 +1,8 @@
+// use crate::errors::ProcessError;
+// use crate::models::{Tag};
+// use crate::requests::{get_and_process_data};
+
+
+// pub async fn get_available_tags() -> Result<Vec<Tag>, ProcessError> {
+//     Ok(get_and_process_data(jar, &"tags?step=100").await?)
+// }
